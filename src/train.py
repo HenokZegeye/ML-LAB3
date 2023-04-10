e7548d1a827d8f2592bdcba16dbb2cf77c665ee8
Henok Birru <henokbirru@Henok.local>
2023-04-10 17:23:26 +0300

    Initial test setup added with lunarlander environment

---
+++ src/train.py
@@ -0,0 +1,53 @@
+import gym
+from stable_baselines3 import PPO, A2C
+import os
+import mlflow
+from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
+import sys
+from utils import MLflowOutputFormat
+
+ALGO_TYPE='A2C'
+EPOCHS = 30
+EXPT_NAME = 'Different Algorithms'
+
+models_dir = f'experiments/models/{ALGO_TYPE}'
+logs_dir = 'logs'
+
+loggers = Logger(
+    folder=None,
+    output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
+)
+
+
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+if __name__ == '__main__':
+
+    mlflow.set_experiment(EXPT_NAME)
+
+    mlflow.start_run(run_name=f"{ALGO_TYPE}-01")
+
+    env = gym.make('LunarLander-v2')
+    
+    model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=logs_dir, device='cpu')
+    
+    model.set_logger(loggers)
+    env.reset()
+
+
+    TIMESTEPS = 10000
+    for i in range(EPOCHS):
+        model.learn(
+            total_timesteps=TIMESTEPS, 
+            reset_num_timesteps=False, 
+            tb_log_name=f'{ALGO_TYPE}'
+            )
+        model.save(f'{models_dir}/{TIMESTEPS*i}')
+
+    mlflow.log_param("algo_type", ALGO_TYPE)
+
+    mlflow.end_run()
+    env.close()
